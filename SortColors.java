797807f68a3978e7b1b233403a9ef56974279fac
Jatin Mehta <jatinmehta11508@gmail.com>
2025-02-19 11:04:04 +0530

    Update SortColors.java

--- SortColors.java
+++ SortColors.java
@@ -1,14 +1,10 @@
-package Package2;
-
-import java.util.Arrays;
-
-public class SortColors {
-    public static void sortColors(int[] nums) {
+class Solution {
+    public void sortColors(int[] nums) {
         int low = 0, mid = 0, high = nums.length - 1;
-        
+
         while (mid <= high) {
             if (nums[mid] == 0) { 
-                swap(nums, mid, low);
+                swap(nums, low, mid);
                 low++;
                 mid++;
             } else if (nums[mid] == 1) { 
@@ -20,20 +16,9 @@ public class SortColors {
         }
     }
 
-    private static void swap(int[] nums, int i, int j) {
+    private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
-    }
-
-    public static void main(String[] args) {
-        int[] nums1 = {2, 0, 2, 1, 1, 0};
-        int[] nums2 = {2, 0, 1};
-
-        sortColors(nums1);
-        sortColors(nums2);
-
-        System.out.println("Sorted nums1: " + Arrays.toString(nums1));
-        System.out.println("Sorted nums2: " + Arrays.toString(nums2));
     }
 }
